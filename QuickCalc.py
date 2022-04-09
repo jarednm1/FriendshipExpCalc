e1e71df3130873d048a79f41186bc57092b86a20
Jared <17jmyers@gmail.com>
2022-04-09 14:19:43 -0400

    Version 1.1.1

--- QuickCalc.py
+++ QuickCalc.py
@@ -10,7 +10,7 @@ while True:
 
         TotalNumberGiven = BestFriends + UltraFriends + GreatFriends + GoodFriends
 
-        if (TotalNumberGiven >= 30)
+        if (TotalNumberGiven >= 30):
             print(GiftWarning)
             print("\n\n")
 
